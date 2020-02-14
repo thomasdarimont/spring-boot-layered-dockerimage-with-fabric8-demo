f26df7eb57a67ef94ef27130c8fabdf88bc6a2e3
Thomas Darimont <thomas.darimont@gmail.com>
2020-02-14 16:28:48 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,4 @@
+Multi-layered Docker Image with Spring Boot 2.3.0+ and fabric8 Maven Docker Plugin
+----
+
+Simple example for producing a multi-layer docker image optimizes docker layer cache usage.
---
+++ src/main/java/demo/App.java
@@ -0,0 +1,26 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.Map;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    Object hello() {
+        return Map.of("Hello", Instant.now());
+    }
+}
